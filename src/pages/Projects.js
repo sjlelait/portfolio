82f31af28c4cf10355ec73ba0cebc10bbbc61a6e
Sara Le Lait <sjlelait@gmail.com>
2023-05-06 14:48:38 -0600

    wip: edits carousel css

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,7 +4,7 @@ import { Container, Carousel } from 'react-bootstrap';
 const Projects = (props) => {
     return (
       <Container className='page-container'>
-        <Carousel className='carousel'>
+        <Carousel className='carousel' variant='dark'>
           <Carousel.Item>
             <div className='image-container'>
               <img
