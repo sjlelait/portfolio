35ccab463ccc7b218b9b442ef2077975032dbca4
Sara Le Lait <sjlelait@gmail.com>
2023-05-09 17:03:35 -0600

    wip: works on about p

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,22 @@ import { Container } from 'react-bootstrap';
 const About = (props) => {
     return (
         <Container className='page-container'>
-            <h1>My Bio</h1>
+            <h1>Sara Le Lait</h1>
+            <h3>Software Developer</h3>
+            <p>As a professional, I've had the opportunity to wear many hats, 
+                from client management to leadership and of course, coding. 
+                My passion lies in creating software that is not only functional but approachable. 
+                I thrive in fast-paced environments and find joy in being creative and adaptive 
+                when solving problems.
+                I bring a unique blend of empathy, patience, and passion to every project I work on. 
+                Understanding the needs of both clients and end-users is crucial to delivering 
+                high-quality software solutions. I am always eager to learn and improve my skills, 
+                and I am excited to contribute to the development of cutting-edge software that has 
+                a meaningful impact. 
+                Joining a dynamic team where I can utilize my skills and collaborate with others 
+                is something I look forward to. 
+                Together, we can create software that not only meets the needs of our clients 
+                but exceeds their expectations.</p>
         </Container>
     )
   };
