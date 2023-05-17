4ac2be6503659ac90874774246b047f5242b60de
Sara Le Lait <sjlelait@gmail.com>
2023-05-16 20:11:33 -0600

    wip: edits form text

--- src/components/Form.js
+++ src/components/Form.js
@@ -41,6 +41,11 @@ function Form(props) {
     return (
         <Container className='my-5'>
             <h3>Contact Sara Le Lait</h3>
+            <h6>
+                Have questions about my resume? Need more information? 
+                Have a great joke to share? 
+                I'll get back to you as soon as I can!
+            </h6>
             < br/>
             <form name="contact" data-netlify="true" method="POST" onSubmit={handleSubmit}>
                 <input type="hidden" name="form-name" value="contact" />
