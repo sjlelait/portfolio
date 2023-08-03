005896c58ab178d022345f173985f34a559ff407
Sara Le Lait <sjlelait@gmail.com>
2023-08-02 18:52:07 -0600

    wip: adds Yadeeda to projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,6 +3,8 @@ import { Container, Carousel, Button, Row } from 'react-bootstrap';
 import disperse from '../assets/disperse.png';
 import onit from '../assets/onit.png';
 import peculiar from '../assets/peculiar-charm.png';
+import yadeeda from '../assets/ydSS.png';
+
 
 const Projects = (props) => {
     return (
@@ -12,6 +14,40 @@ const Projects = (props) => {
         </Row>
         < br/>
         <Carousel className='carousel' interval='9000' variant='dark'>
+          <Carousel.Item>
+            <div className='image-container'>
+              <img
+                className='d-block mx-auto img-fluid projectImage'
+                src={yadeeda}
+                alt='Yadeeda'
+              />
+            </div>
+            <Carousel.Caption>
+              <h1 className='project-title'>Yadeeda</h1>
+              <h5>Client website to showcase songs to learn French.</h5>
+              <p>
+                Yadeeda integrates client's previous website into a new interactive React website.
+                Purposed to emphasize rediretion to client's YouTube channel.
+                Users can view dynamic content from YouTube and view all albums with print-friendly song lyrics.
+                I utilized Tailwind to improve user engagement and to highlight the Yadeeda brand, modernized using Canva.
+              </p>
+              <div className='d-flex justify-content-center'>
+                <Button
+                  variant='dark' size='sm' 
+                  href='https://yadeeda.com/'
+                  target='_blank' 
+                  rel='noopener noreferrer'
+                  style={{marginRight: '1rem'}}>Deployed App
+                </Button>
+                <Button
+                  variant='dark' size='sm' 
+                  href='https://github.com/sjlelait/yadeeda'
+                  target='_blank' 
+                  rel='noopener noreferrer'>GitHub
+                </Button>
+              </div>              
+            </Carousel.Caption>
+          </Carousel.Item>
           <Carousel.Item>
             <div className='image-container'>
               <img
