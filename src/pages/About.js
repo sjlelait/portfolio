c0837252dd2fb9dfc88f4a8027506dd7750fb257
Sara Le Lait <sjlelait@gmail.com>
2024-02-03 09:47:18 -0700

    fix: edits about & project descriptions

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,35 +5,34 @@ import profile from '../assets/profile-img.jpg';
 const About = (props) => {
     return (
         <Container className='page-container'>
-            <img className='profile-img'src={profile} alt="Sara Le Lait" />
+            <img className='profile-img' src={profile} alt="Sara Le Lait" />
             <h1>Sara Le Lait</h1>
             <h3 className='about-p'>Software Developer</h3>
-            <p className='about-p'>As a professional, I've had the opportunity to wear many hats, 
-                from client management to leadership and of course, coding. 
-                My passion lies in creating software that is not only functional but approachable. 
-                I thrive in fast-paced environments and find joy in being creative and adaptive 
+            <p className='about-p'>As a professional, I've had the opportunity to wear many hats,
+                from client management to leadership and of course, coding.
+                My passion lies in creating software that is not only functional but approachable.
+                I thrive in fast-paced environments and find joy in being creative and adaptive
                 when solving problems.
-                I bring a unique blend of empathy, patience, and passion to every project I work on. 
-                Understanding the needs of both clients and end-users is crucial to delivering 
-                high-quality software solutions.</p>
-                <p className='about-p'>I am always eager to learn and improve my skills, 
-                and I am excited to contribute to the development of cutting-edge software that has 
-                a meaningful impact. 
-                Joining a dynamic team where I can utilize my skills and collaborate with others 
-                is something I look forward to. 
-                Together, we can create software that not only meets the needs of our clients 
+                I bring a unique blend of empathy, patience, and passion to every project I work on.
+                I understand that delivering high-quality software solutions requires a deep understanding of
+                the needs of both clients and end users.</p>
+            <p className='about-p'>I am always eager to learn and improve my skills,
+                and I am excited to contribute to the development of cutting-edge software that has
+                a meaningful impact.
+                I look forward to joining a dynamic team where I can utilize my skills and collaborate with others.
+                Together, we can create software that not only meets the needs of our clients
                 but exceeds their expectations.</p>
-                < br/>
-                <h5>See my full resume{' '}
-                <Button 
-                    variant='dark' size='sm' 
+            < br />
+            <h5>See my full resume{' '}
+                <Button
+                    variant='dark' size='sm'
                     href='https://docs.google.com/document/d/159n7Miyf09AbafWpN35wPPPHh6WZMlSkJF03pfsyXZo/edit?usp=sharing'
-                    target='_blank' 
+                    target='_blank'
                     rel='noopener noreferrer'>HERE
-                    </Button>
-                </h5>
+                </Button>
+            </h5>
         </Container>
     )
-  };
-  
+};
+
 export default About;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,150 +7,150 @@ import yadeeda from '../assets/ydSS.png';
 
 
 const Projects = (props) => {
-    return (
-      <Container className='page-container'>
-        <Row className='title-row'>
+  return (
+    <Container className='page-container'>
+      <Row className='title-row'>
         <h2>Featured Projects</h2>
-        </Row>
-        < br/>
-        <Carousel className='carousel' interval='9000' variant='dark'>
-          <Carousel.Item>
-            <div className='image-container'>
-              <img
-                className='d-block mx-auto img-fluid projectImage'
-                src={yadeeda}
-                alt='Yadeeda'
-              />
+      </Row>
+      < br />
+      <Carousel className='carousel' interval='9000' variant='dark'>
+        <Carousel.Item>
+          <div className='image-container'>
+            <img
+              className='d-block mx-auto img-fluid projectImage'
+              src={yadeeda}
+              alt='Yadeeda'
+            />
+          </div>
+          <Carousel.Caption>
+            <h1 className='project-title'>Yadeeda</h1>
+            <h5>Client website to showcase songs to learn French.</h5>
+            <p>
+              Yadeeda integrates client's previous website into a new interactive React-based site.
+              Purposed to emphasize rediretion to the client's YouTube channel.
+              Users can now view dynamic content from YouTube and view all albums with print-friendly song lyrics.
+              I utilized Tailwind to improve user engagement and to highlight the Yadeeda brand, modernized using Canva.
+            </p>
+            <div className='d-flex justify-content-center'>
+              <Button
+                variant='dark' size='sm'
+                href='https://yadeeda.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ marginRight: '1rem' }}>Deployed App
+              </Button>
+              <Button
+                variant='dark' size='sm'
+                href='https://github.com/sjlelait/yadeeda'
+                target='_blank'
+                rel='noopener noreferrer'>GitHub
+              </Button>
             </div>
-            <Carousel.Caption>
-              <h1 className='project-title'>Yadeeda</h1>
-              <h5>Client website to showcase songs to learn French.</h5>
-              <p>
-                Yadeeda integrates client's previous website into a new interactive React website.
-                Purposed to emphasize rediretion to client's YouTube channel.
-                Users can view dynamic content from YouTube and view all albums with print-friendly song lyrics.
-                I utilized Tailwind to improve user engagement and to highlight the Yadeeda brand, modernized using Canva.
-              </p>
-              <div className='d-flex justify-content-center'>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://yadeeda.com/'
-                  target='_blank' 
-                  rel='noopener noreferrer'
-                  style={{marginRight: '1rem'}}>Deployed App
-                </Button>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://github.com/sjlelait/yadeeda'
-                  target='_blank' 
-                  rel='noopener noreferrer'>GitHub
-                </Button>
-              </div>              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className='image-container'>
-              <img
-                className='d-block mx-auto img-fluid projectImage'
-                src={disperse}
-                alt='Disperse'
-              />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='image-container'>
+            <img
+              className='d-block mx-auto img-fluid projectImage'
+              src={disperse}
+              alt='Disperse'
+            />
+          </div>
+          <Carousel.Caption>
+            <h1 className='project-title'>Disperse</h1>
+            <h5>A tool for wilderness lovers and adventurers.</h5>
+            <p>
+              Disperse allows users to login with Django's built-in authentication and manage their preferred camping spots.
+              The application features a "find my location" option that integrates a comprehensive map showcasing the area.
+              Additionally, Disperse incorporates dynamic local weather data sourced from WeatherAPI and offers photo upload capabilities.
+            </p>
+            <div className='d-flex justify-content-center'>
+              <Button
+                variant='dark' size='sm'
+                href='https://disperse.herokuapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ marginRight: '1rem' }}>Deployed App
+              </Button>
+              <Button
+                variant='dark' size='sm'
+                href='https://github.com/sjlelait/disperse'
+                target='_blank'
+                rel='noopener noreferrer'>GitHub
+              </Button>
             </div>
-            <Carousel.Caption>
-              <h1 className='project-title'>Disperse</h1>
-              <h5>A tool for wilderness lovers and adventurers.</h5>
-              <p>
-                User's can login with Django's built-in authentication to manage their preferred camping spots. 
-                Disperse features a "find my location" feature that integrates a comprehensive map showcasing the area.
-                Additionally, Disperse incorporates dynamic local weather data sourced from WeatherAPI and photo upload capabilities.
-              </p>
-              <div className='d-flex justify-content-center'>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://disperse.herokuapp.com/'
-                  target='_blank' 
-                  rel='noopener noreferrer'
-                  style={{marginRight: '1rem'}}>Deployed App
-                </Button>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://github.com/sjlelait/disperse'
-                  target='_blank' 
-                  rel='noopener noreferrer'>GitHub
-                </Button>
-              </div>              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className='image-container'>
-              <img
-                className='d-block mx-auto img-fluid projectImage'
-                src={onit}
-                alt='onit'
-              />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='image-container'>
+            <img
+              className='d-block mx-auto img-fluid projectImage'
+              src={onit}
+              alt='onit'
+            />
+          </div>
+          <Carousel.Caption>
+            <h1 className='project-title'>onit</h1>
+            <h5>A collaborative project:</h5>
+            <p>
+              A task Management tool for the go-getter who wants to maintain balance and self-care.
+              Users will log-in through Google Firebase to create and manage their own personal to-do lists
+              using full CRUD operations. I assumed the position of Lead Backend and Database Manager while actively
+              contributing to front-end feature development.
+            </p>
+            <div className='d-flex justify-content-center'>
+              <Button
+                variant='dark' size='sm'
+                href='https://onit-app.netlify.app/'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ marginRight: '1rem' }}>Deployed App
+              </Button>
+              <Button
+                variant='dark' size='sm'
+                href='https://github.com/sjlelait/onit-frontend'
+                target='_blank'
+                rel='noopener noreferrer'>GitHub
+              </Button>
             </div>
-            <Carousel.Caption>
-              <h1 className='project-title'>onit</h1>
-              <h5>A collaborative project:</h5>
-              <p>
-                A task Management tool for the go-getter who wants to maintain balance and self-care.
-                Users will log-in through Google Firebase to create and manage their own personal to-do lists 
-                using full CRUD operations. I assumed the position of Lead Backend and Database Manager while also
-                contributing to front-end feature development.
-              </p>
-              <div className='d-flex justify-content-center'>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://onit-app.netlify.app/'
-                  target='_blank' 
-                  rel='noopener noreferrer'
-                  style={{marginRight: '1rem'}}>Deployed App
-                </Button>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://github.com/sjlelait/onit-frontend'
-                  target='_blank' 
-                  rel='noopener noreferrer'>GitHub
-                </Button>
-              </div>              
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className='image-container'>
-              <img
-                className='d-block mx-auto img-fluid projectImage'
-                src={peculiar}
-                alt='Peculiar Charm'
-              />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='image-container'>
+            <img
+              className='d-block mx-auto img-fluid projectImage'
+              src={peculiar}
+              alt='Peculiar Charm'
+            />
+          </div>
+          <Carousel.Caption>
+            <h1 className='project-title'>Peculiar Charm</h1>
+            <h5>A Gallery of the Unique and Odd.</h5>
+            <p>
+              A comprehensive full-stack application using Node.js, Express and MongoDB, encompassing
+              all aspects of CRUD operations. The application provides secure login functionality that allows
+              users to seamlessly access, modify, contrubute, and remove content within the collection.
+            </p>
+            <div className='d-flex justify-content-center'>
+              <Button
+                variant='dark' size='sm'
+                href='https://peculiar-charm.herokuapp.com/'
+                target='_blank'
+                rel='noopener noreferrer'
+                style={{ marginRight: '1rem' }}>Deployed App
+              </Button>
+              <Button
+                variant='dark' size='sm'
+                href='https://github.com/sjlelait/peculiar-charm'
+                target='_blank'
+                rel='noopener noreferrer'>GitHub
+              </Button>
             </div>
-            <Carousel.Caption>
-              <h1 className='project-title'>Peculiar Charm</h1>
-              <h5>A Gallery of the Unique and Odd.</h5>
-              <p>
-                A comprehensive full-stack application using Node.js, Express and MongoDB, encompassing 
-                all aspects of CRUD operations. This application empoweres users with secure login functionality
-                to seamlessly access, modify, contrubute to, and remove content within the collection.
-              </p>
-              <div className='d-flex justify-content-center'>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://peculiar-charm.herokuapp.com/'
-                  target='_blank' 
-                  rel='noopener noreferrer'
-                  style={{marginRight: '1rem'}}>Deployed App
-                </Button>
-                <Button
-                  variant='dark' size='sm' 
-                  href='https://github.com/sjlelait/peculiar-charm'
-                  target='_blank' 
-                  rel='noopener noreferrer'>GitHub
-                </Button>
-              </div>           
-            </Carousel.Caption>
-          </Carousel.Item>
+          </Carousel.Caption>
+        </Carousel.Item>
       </Carousel>
     </Container>
-    )
-  };
-  
+  )
+};
+
 export default Projects;
