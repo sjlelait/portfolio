4cf26d5748ce4872abd4330514754ec895fd2372
Sara Le Lait <sjlelait@gmail.com>
2024-03-22 18:08:55 -0600

    wip: edits about <p>

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,20 +8,26 @@ const About = (props) => {
             <img className='profile-img' src={profile} alt="Sara Le Lait" />
             <h1>Sara Le Lait</h1>
             <h3 className='about-p'>Software Developer</h3>
-            <p className='about-p'>As a professional, I've had the opportunity to wear many hats,
+            <p className='about-p'>
+                As a professional, I've had the opportunity to wear many hats,
                 from client management to leadership and of course, coding.
                 My passion lies in creating software that is not only functional but approachable.
-                I thrive in fast-paced environments and find joy in being creative and adaptive
+                By intertwining artistic vision with a minimalist approach, I strive to create user-friendly
+                experiences marked by simplicity.
+                Thriving in fast-paced environments, I find joy in being creative and adaptive
                 when solving problems.
-                I bring a unique blend of empathy, patience, and passion to every project I work on.
-                I understand that delivering high-quality software solutions requires a deep understanding of
-                the needs of both clients and end users.</p>
-            <p className='about-p'>I am always eager to learn and improve my skills,
+                A unique blend of empathy, patience, and zeal follows me into every project I work on.
+                This helps me to deliver high-quality software solutions the meet the needs of both
+                clients and end users.
+            </p>
+            <p className='about-p'>
+                I am always eager to learn and improve my skills,
                 and I am excited to contribute to the development of cutting-edge software that has
                 a meaningful impact.
                 I look forward to joining a dynamic team where I can utilize my skills and collaborate with others.
                 Together, we can create software that not only meets the needs of our clients
-                but exceeds their expectations.</p>
+                but exceeds their expectations.
+            </p>
             < br />
             <h5>See my full resume{' '}
                 <Button
